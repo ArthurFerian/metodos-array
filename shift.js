6e35ef97b2fcf46b16b1ca32e6d94c2fcab2aa9b
Arthur Ferian <arthurferian.17@gmail.com>
2025-05-23 08:51:01 -0300

    Adiciona arquivo de metodos array

---
+++ shift.js
@@ -0,0 +1,10 @@
+let playlist = ["funk", "trap", "rap"];
+console.log(`antes do shift: ${playlist}`);
+
+// removendo a primeira música da playlist
+let musicaRemovida1= playlist.shift();
+
+
+console.log(`Depois do shift: ${playlist}`);
+console.log(`Música removida: ${musicaRemovida1}`);
+
