c230a64a9bf99b9f4f8a7db82b4554c21fbb2b10
Arthur Ferian <arthurferian.17@gmail.com>
2025-05-23 10:08:14 -0300

    Altera um arquivo slice

--- slice.js
+++ slice.js
@@ -2,5 +2,6 @@ let playlist = ['Funk', 'Pop', 'Jazz', 'Reggae', 'Classical', 'Indie'];
 
 console.log(`Parte da playlist (índices 2 a 5): ${playlist.slice(2, 6)}`);
 
-
 console.log(`Parte da playlist (índices 0 a 3): ${playlist.slice(0, 4)}`);
+
+console.log(`Parte da playlist (índices 0 a 1): ${playlist.slice(0,1)}`);
