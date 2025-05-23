089c565d5b8a990af4bc1949ebc157589263d3a2
Arthur Ferian <arthurferian.17@gmail.com>
2025-05-23 08:55:07 -0300

    Adiciona arquivo de metodos array

---
+++ unshit.js
@@ -0,0 +1,8 @@
+let playlist = ["funk", "trap", "rap"];
+console.log(`antes do unshift: ${playlist}`);
+
+// adicionando uma nova música no início da playlist
+playlist.unshift("Rock");
+
+
+console.log(`Depois do unshift: ${playlist}`);
