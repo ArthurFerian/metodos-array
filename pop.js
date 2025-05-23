05c458a0b70b668f4f92ef6ab651c2649a25aaf8
Arthur Ferian <arthurferian.17@gmail.com>
2025-05-23 08:33:43 -0300

    Adiciona arquivo de metodos array

---
+++ pop.js
@@ -0,0 +1,14 @@
+let playlist = ["funk", "trap", "rap"];
+console.log(`antes do pop: ${playlist}`);
+
+// Adicionando uma nova música á playlist
+let musicaRemovida = playlist.pop();
+let musicaRemovidaa = playlist.pop();
+
+console.log(`Depois do pop: ${playlist}`);
+console.log(`Música removida: ${musicaRemovida}`);
+console.log(`Segunda música removida: ${musicaRemovidaa}`);
+
+
+
+
---
+++ push.js
@@ -0,0 +1,7 @@
+let playlist = ["funk", "trap", "rap"];
+console.log("antes do push:", playlist);
+
+// Adicionando uma nova música á playlist
+playlist.push("trap");
+
+console.log("Depois do push:", playlist);
